1747a21f47d813c3486f6e1b5b59ac8bd4068983
itValenok <126165057+itValenok@users.noreply.github.com>
2024-07-29 14:50:22 +0800

    1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Джими", "Хендрикс", (byte) 27);
+        userService.saveUser("Джими", "Моррисон", (byte) 27);
+        userService.saveUser("Дженис", "Джоплин", (byte) 27);
+        userService.saveUser("Курт", "Кобейн", (byte) 27);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
